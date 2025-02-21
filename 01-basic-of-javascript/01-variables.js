78283992a2750cae0630de6c38f27339e47ccae7
@Raju_Kumar <97250356+Raju269@users.noreply.github.com>
2025-02-21 15:00:58 +0000

    working on variale let , var ,const

---
+++ 01-basic-of-javascript/01-variables.js
@@ -0,0 +1,16 @@
+const accountID = 123434;
+let accountEmail = "rajuk631149@gmail.com";
+var accountPassword = "32349";
+accountCity = "New Delhi";
+let accountStates;
+// accountID = 23343;
+console.log(accountID);
+accountEmail = "physicsnecrt@gmail.com";
+accountPassword = "12345";
+accountCity = "Pune";
+
+console.table([accountEmail,accountID,accountPassword,accountCity,accountStates]);
+
+// Prefer not to use var 
+// because of issue in block scope and functional scope 
+
