0b494e22996a816b9d3319b1dbeb34c1a4b114d9
@Raju_Kumar <97250356+Raju269@users.noreply.github.com>
2025-02-21 15:29:11 +0000

    working on conversion in javscript number to string etc.

---
+++ 01-basic-of-javascript/03-conversion-operators-in-javascript.js
@@ -0,0 +1,28 @@
+let score = 'abc';
+
+console.log(typeof score);
+
+let valueInNUmber = Number(score)
+
+console.log(typeof valueInNUmber);
+
+console.log(valueInNUmber);
+//  convert number to string , boolean , etc. 
+// 22 = number 
+// '22' => NAN not a number
+// true => 1 , false => 0 
+
+
+let isLoggedIn = 1;
+let booleanLoggedIn = Boolean(isLoggedIn);
+console.log(booleanLoggedIn);
+console.log(typeof booleanLoggedIn);
+// investagation study is 
+// 1 => true , 0 => false,
+//  "" => false
+//  "hitesh" => true 
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
