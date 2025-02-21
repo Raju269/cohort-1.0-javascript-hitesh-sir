41abd55eab53b5e7aced8e12693fbb2c69ced95b
@Raju_Kumar <97250356+Raju269@users.noreply.github.com>
2025-02-21 15:11:24 +0000

    Working on dataypes in javascript

---
+++ 01-basic-of-javascript/02-datatype-.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code  as newer version 
+
+// alert ("linked file ") we are using node.js , not browser
+
+// code readability should be high 
+console.log("Hello ji ");
+console.log("Raju ji ");
+
+// Datatypes in javascript
+// Data type in javascript 
+// Primtive data type in javascript
+// string   data type in javascript
+// Number data type in javascript
+// Null data type in javascript
+// Boolean data type in javascript
+// undefined data type in javascript
+let mydatatype = 'string';
+let number = 23;
+let boolean = false;
+let xyz = null;
+let a ;
+console.log(mydatatype);
+console.log(number);
+console.log(xyz);
+console.log(boolean);
+console.log(a);
+
+// Dynamic data type in javascript
+let a1 = 5; 
+console.log(a);
+a  = 'rajukumar';
+console.log(a1);
+console.table([mydatatype,number,xyz,boolean,a,a1]);
