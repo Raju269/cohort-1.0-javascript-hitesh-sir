ab3de58815ebf4540baa749482a5fe7736e7a44e
@Raju_Kumar <97250356+Raju269@users.noreply.github.com>
2025-02-22 08:21:40 +0000

    working on string and operation to used

---
+++ 05_string-in-javascript.js
@@ -0,0 +1,34 @@
+let firstName = "Raju";
+let lastName = "Kumar";
+// console.log(firstName + lastName + "Prapati");
+
+// string ` string it pollation `
+// console.log(`Hello My name is ${firstName} and my last name is ${lastName}`);
+
+// 
+const gameName = new String("       Clash of       clane");
+
+// console.log(gameName);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.__proto__.String);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.charAt(4));
+// console.log(gameName.charAt(6));
+// console.log(gameName.charAt(9));
+// console.log(gameName.concat(firstName,lastName));
+// console.log(gameName.indexOf('h'));
+// console.log(gameName.lastIndexOf('e'));
+// console.log(gameName.substring(0,8));
+// console.log(gameName.substring(2,12));
+// console.log(gameName.slice(-1,9));
+// console.log(gameName.trim( ));
+
+const url = "www.google.com";
+console.log(url.replace('www','https'));
+console.log(url.includes("google"));
+console.log(url.split('-'));
