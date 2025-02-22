f4b940de3f72c3f9759aee21e3288929c4fe767e
@Raju_Kumar <97250356+Raju269@users.noreply.github.com>
2025-02-22 04:35:56 +0000

    addingg some work on operations in conversion

--- 01-basic-of-javascript/03-conversion-operators-in-javascript.js
+++ 01-basic-of-javascript/03-conversion-operators-in-javascript.js
@@ -26,3 +26,24 @@ let someNumber = 33;
 let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+// ******************* Operations ***********************
+let value = 3;
+let negitiveValue = -value;
+console.log(negitiveValue);
+
+let str1 = "Raju ";
+let str2 = "Kumar"
+let str3 = str1+str2;
+console.log(str3);
+console.log("1" + 2 );
+console.log(1 + "2");
+console.log("1" + 2 +2 );
+console.log(1+2+"2");
+
+console.log((3+4)*(5%3));
+console.log(+true)
+console.log(+"");
+console.log(-false);
+
